0e0265069f742a8c69e33e3131f36b733c973f8f
Vasudev290 <vasukesavulu@gmail.com>
2025-09-26 17:38:31 +0530

    feat: chages

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: [
+    'http://localhost:3000',
+    'https://voice-enabled-ai-chatbot-client.vercel.app'
+  ], 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
